935972eda97ea6444603753f6784207e7cf0f705
SergioGBosco <sergiogibosco@gmail.com>
2025-07-09 16:03:30 +0200

    feat: add function for change src

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-//creo la funzione legata all'html che mi permetta di modifica l'immagine ogni qualvolta viene premuto l'interruttore 
+//definisco le variabili e le collego a gli elementi prensenti all'interno dell'html 
+const lamp = document.getElementById(`lamp`)
+const button = document.getElementById(`switch`)
+
+//creo la funzione che mi permette di cliccare il bottone e contemporaneamente questo mi cambia l'immagine 
+button.addEventListener(`click`, function () {
+
+  //con questa funzione vado a modificare direttamente il valore del src cosi da alternare l'immagine
+  //creo la condizione per permettere alla lampadina di essere accesa o spenta ad ogni click
+
+  lamp.src = (`img/yellow_lamp.png`)
+
+  // if (lamp.src.contains(`img/yellow_lamp.png`)) {
+  //   lamp.src.add(`img/white_lamp.png`);
+  //   lamp.src.remove(`img/yellow_lamp.png`)
+  // }
+  // else (lamp.src.contains(`img/white_lamp.png`))
+  // lamp.src.add(`img/yellow_lamp.png`);
+  // lamp.src.remove(`img/white_lamp.png`)
+
+
+
+})
